07f6cf4a4cddf7a5425a32be8e53456895c0fad8
James Choca <jchoca@gmail.com>
2020-09-05 11:28:27 -0700

    wip

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -1,5 +1,6 @@
 defmodule GameTest do
   use ExUnit.Case
+  require IEx
 
   alias Hangman.Game
 
@@ -43,21 +44,48 @@ defmodule GameTest do
   end
 
   test "a guessed word is a won game" do
+    moves = [
+      {"w", :good_guess},
+      {"i", :good_guess},
+      {"b", :good_guess},
+      {"l", :good_guess},
+      {"e", :won}
+    ]
+    Enum.reduce(moves, Game.new_game("wibble"), fn {guess, state}, game ->
+      { new_game, _tally } = Game.make_move(game, guess)
+      assert new_game.game_state == state
+      new_game
+    end)
+  end
+
+  test "bad guess is recognized" do
     game = Game.new_game("wibble")
-    {game, _tally} = Game.make_move(game, "w")
-    assert game.game_state == :good_guess
-    assert game.turns_left == 7
-    {game, _tally} = Game.make_move(game, "i")
-    assert game.game_state == :good_guess
-    assert game.turns_left == 7
-    {game, _tally} = Game.make_move(game, "b")
-    assert game.game_state == :good_guess
-    assert game.turns_left == 7
-    {game, _tally} = Game.make_move(game, "l")
-    assert game.game_state == :good_guess
-    assert game.turns_left == 7
-    {game, _tally} = Game.make_move(game, "e")
-    assert game.game_state == :won
-    assert game.turns_left == 7
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+
+  test "lost game is recognized" do
+    game = Game.new_game("w")
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+    { game, _tally } = Game.make_move(game, "b")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 5
+    { game, _tally } = Game.make_move(game, "c")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 4
+    { game, _tally } = Game.make_move(game, "d")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 3
+    { game, _tally } = Game.make_move(game, "e")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 2
+    { game, _tally } = Game.make_move(game, "f")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 1
+    { game, _tally } = Game.make_move(game, "g")
+    assert game.game_state == :lost
   end
 end
