0627174f2a95bf308b043f128952d8354f0415d7
James Choca <jchoca@gmail.com>
2020-10-03 15:29:53 -0700

    added text client

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -13,9 +13,12 @@ defmodule Hangman.Game do
   end
 
   def new_game() do
-    new_game(Dictionary.random_word) 
+    new_game(Dictionary.random_word)
   end
 
+  @spec make_move(map, any) :: %{
+          game_state: :already_used | :bad_guess | :good_guess | :lost | :won
+        }
   def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
     game
   end
@@ -32,7 +35,7 @@ defmodule Hangman.Game do
     }
   end
 
-  ############################################################################# 
+  #############################################################################
 
   defp accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
@@ -55,7 +58,7 @@ defmodule Hangman.Game do
   end
 
   defp score_guess(game = %{ turns_left: turns_left}, _not_good_guess) do
-    %{ game | 
+    %{ game |
        game_state: :bad_guess,
        turns_left: turns_left - 1
       }
@@ -63,6 +66,7 @@ defmodule Hangman.Game do
 
   defp reveal_guessed(letters, used) do
     letters
+    |> IO.inspect()
     |> Enum.map(fn letter -> reveal_letter(letter, MapSet.member?(used, letter)) end)
   end
 
