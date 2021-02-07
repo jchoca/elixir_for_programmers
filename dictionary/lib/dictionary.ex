44209288f8c877136a71cbbce4ba72ff7de0081c
Jimmy Choca <jchoca@gmail.com>
2021-02-07 10:39:57 -0700

    wip

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,4 +1,4 @@
 defmodule Dictionary do
-  defdelegate start(), to: Dictionary.WordList
+  defdelegate start_link(), to: Dictionary.WordList
   defdelegate random_word(words), to: Dictionary.WordList
 end
--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -1,17 +1,16 @@
 defmodule Dictionary.WordList do
+  def start_link() do
+    Agent.start_link(&word_list/0)
+  end
+
   defp word_list() do
     "../../assets/words.txt"
     |> Path.expand(__DIR__)
     |> File.read!()
-    |> String.split(~r/\n/)
+    |> String.split(~r/\r\n/)
   end
 
-  def random_word(words) do
-    words
-    |> Enum.random()
-  end
-
-  def start() do
-    word_list()
+  def random_word(agent) do
+    Agent.get(agent, &Enum.random/1)
   end
 end
