ea5d8de706644eece341a93ea1c19aaa1b557aaf
Jimmy Choca <jchoca@gmail.com>
2021-08-26 13:23:36 -0700

    get socket/channel communication working

--- socket_gallows/lib/socket_gallows/application.ex
+++ socket_gallows/lib/socket_gallows/application.ex
@@ -1,32 +1,32 @@
 defmodule SocketGallows.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
+    # See https://hexdocs.pm/elixir/Application.html
+    # for more information on OTP Applications
+    @moduledoc false
 
-  use Application
+    use Application
 
-  def start(_type, _args) do
-    children = [
-      # Start the Telemetry supervisor
-      SocketGallowsWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: SocketGallows.PubSub},
-      # Start the Endpoint (http/https)
-      SocketGallowsWeb.Endpoint
-      # Start a worker by calling: SocketGallows.Worker.start_link(arg)
-      # {SocketGallows.Worker, arg}
-    ]
+    def start(_type, _args) do
+        children = [
+            # Start the Telemetry supervisor
+            SocketGallowsWeb.Telemetry,
+            # Start the PubSub system
+            {Phoenix.PubSub, name: SocketGallows.PubSub},
+            # Start the Endpoint (http/https)
+            SocketGallowsWeb.Endpoint
+            # Start a worker by calling: SocketGallows.Worker.start_link(arg)
+            # {SocketGallows.Worker, arg}
+        ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: SocketGallows.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
+        # See https://hexdocs.pm/elixir/Supervisor.html
+        # for other strategies and supported options
+        opts = [strategy: :one_for_one, name: SocketGallows.Supervisor]
+        Supervisor.start_link(children, opts)
+    end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
-  def config_change(changed, _new, removed) do
-    SocketGallowsWeb.Endpoint.config_change(changed, removed)
-    :ok
-  end
+    # Tell Phoenix to update the endpoint configuration
+    # whenever the application is updated.
+    def config_change(changed, _new, removed) do
+        SocketGallowsWeb.Endpoint.config_change(changed, removed)
+        :ok
+    end
 end
--- socket_gallows/lib/socket_gallows_web/controllers/page_controller.ex
+++ socket_gallows/lib/socket_gallows_web/controllers/page_controller.ex
@@ -1,7 +1,7 @@
 defmodule SocketGallowsWeb.PageController do
-  use SocketGallowsWeb, :controller
+    use SocketGallowsWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
+    def index(conn, _params) do
+        render(conn, "index.html")
+    end
 end
